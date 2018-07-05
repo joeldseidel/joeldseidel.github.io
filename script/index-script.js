e2b6cf2c3916f6e3660e7150c77a8bb4aee51d7b
Joel Seidel <joeldseidel@gmail.com>
2018-07-05 15:59:46 -0400

    My work button didnt work, now it does
    Readded the script link that I forgot last night

--- script/index-script.js
+++ script/index-script.js
@@ -1,36 +1,7 @@
 var image;
 var namePlate;
 $(document).ready(function(){
-    image = $('.name-photo');
-    namePlate = $('#name-plate-center');
-    setImageHeight(image, namePlate);
-    setTextMargin(image, namePlate);
     $('.work-button').click(function(){
         window.location.href = "mywork.html";
     });
 });
-
-$(window).resize(function(){
-    if(window.matchMedia('(max-width: 576px)').matches) {
-        setImageHeight();
-        setTextMargin();
-    } else {
-        //setImageHeight();
-        setTextMargin();
-    }
-});
-
-function setImageHeight(){
-    var namePlateHeight = namePlate.outerHeight();
-    var newImageHeight = namePlateHeight * 1.5;
-    image.css('height', newImageHeight);
-}
-
-function setTextMargin(){
-    var imageSize = image.outerHeight();
-    var containerSidePadding = namePlate.css('padding-left');
-    containerSidePadding = containerSidePadding.replace('px', '');
-    var totalSideMargin = imageSize + containerSidePadding * 2;
-    console.log(totalSideMargin);
-    $('.text-column').css('margin-left', totalSideMargin);
-}
