51d1ccb81623f260c484ad1200a0757e30ad2145
Joel Seidel <jds6294@psu.edu>
2019-10-09 21:22:26 -0400

    Technical overview screen

--- script/pages-script.js
+++ script/pages-script.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
-    $('#scroll-button').click(function(e){
+    $('.scroll-button').click(function(e){
         e.preventDefault();
         var sectionTo = $(this).attr('href');
         $('html, body').animate({
             scrollTop: $(sectionTo).offset().top
         }, 500);
+    });
+    $('.mouseover-icon-listing').hover(function(e){
+        e.preventDefault();
+        var listingBody = e.find('.icon-listing-body');
     });
 });
